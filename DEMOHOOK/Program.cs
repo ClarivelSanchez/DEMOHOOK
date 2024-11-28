2f95550dd8749e7e3479f567fb86dc232bf2e29c
Clarivel Sánchez Díaz <clarivelsanchez68@gmail.com>
2024-11-28 14:16:38 +0100

    Demostracion de Hook formato de codigo

--- DEMOHOOK/Program.cs
+++ DEMOHOOK/Program.cs
@@ -5,11 +5,11 @@ Console.WriteLine("Prueba de formateo de código");
 int edad = 25;
 
 if (edad >= 18)
-    {
+{
     Console.WriteLine("Es mayor de edad");
-        }
+}
 else
-    {
+{
     Console.WriteLine("Es menor de edad");
-        }
+}
 
